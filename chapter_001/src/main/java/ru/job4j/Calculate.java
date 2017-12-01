701ecb622926c82614e9d3debb6bc15288200077
glin94 <narikus94@gmail.com>
2017-12-01 16:40:59 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,21 @@
 package ru.job4j;
 
-public class Calculate
-{
 
+/**
+ * Class Calculate для отображения Hello World!
+ * @author giniyatov
+ * @since 01.12.2017
+ * @version 1
+ */
+public class Calculate {
+
+/**
+ * Конструктор, вывод строки в консоли
+ * @param arg-arg.
+ */
 public static void main (String [] args)
 	{
 		System.out.println("Hello World!");
-
+	
 	}
 }
